da3696cf39d9603d07beff3cabddf2bf961a585d
madhav-fied <madhavcv2116@gmail.com>
2024-03-22 11:54:26 +0530

    add simple wasm-support

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import init, { greet } from '../maze-gen-wasm/pkg/maze_generator_rust';
+
 const App = () => {
+  useEffect(() => {
+    init().then(() => {
+      greet('Done');
+    });
+  }, []);
+
   return (
     <h1>
       Maze generator
